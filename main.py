37749b96e30e22d73b3014e4159c5d5b5d8b7cf4
Zak00b <abderrazzak.bajjou@artefact.com>
2024-11-26 13:42:29 +0000

    new dev

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ import functions_framework
 
 
 @functions_framework.http
-def main():
-    return Response(json.dumps("Hello World"), 200, mimetype="application/json")
+def main(request):
+    request_json = request.get_json()
+
+    # Retrieve sessionInfo, or initialize it if not present
+    txt = request_json.get("name", {})
+
+    return Response(json.dumps(txt), 200, mimetype="application/json")
